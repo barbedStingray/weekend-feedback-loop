c1a38e1dff9cf41074d67588e2526c936e39bac7
Ben Rehmann <brehmann@me.com>
2023-11-11 13:58:16 -0600

    router added

---
+++ server/Routes/review.router.js
@@ -0,0 +1,16 @@
+
+// ** Router for the Review Page
+
+// imports
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool.js');
+
+// POST response /review
+
+
+
+
+// exports
+
+module.exports = router;
